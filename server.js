58586f58e54aa6f872be3d27c02628c63095ee44
federicowoodward <woodfederico@gmail.com>
2022-11-05 23:43:14 -0300

    firebase agregado

--- server.js
+++ server.js
@@ -2,32 +2,55 @@ import express from 'express';
 import cors from 'cors';
 import routesProducts from './src/routes/routesProducts.js';
 import routesCarts from './src/routes/routesCarts.js';
-import { dbConnect } from "./src/persistencia/dbConnect.js";
-const servidor = express()
+import { dbMN, dbFB } from "./src/persistencia/dbConnect.js";
+import cookieParser from 'cookie-parser';
+const app = express()
 
+const info = {
+    nombre: 'fede',
+    apellidos: 'woodward'
+}
 
 // config basica:
-servidor.use(cors());
-servidor.use(express.json())
-servidor.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-//  rutas inicializar:
-servidor.use("/", routesProducts)
-servidor.use("/cart", routesCarts)
+app.get("/cookies", (req, res) => {
+    res.cookie(`primeraPrueba`, info.nombre, { maxAge: 15000 }).send(`cokie guardada!`)
+})
+
+app.use("/", routesProducts)
+app.use("/cart", routesCarts)
+
 
 const PORT = process.env.PORT || 8080;
-
-try {
-    await dbConnect()
-    console.log("Connected to database")
-
-    servidor.listen(PORT, () => {
+const listen = () => {
+    app.listen(PORT, () => {
         console.log("--------------------------------------------")
         console.log(`Escuchando en ${PORT}`);
         console.log("--------------------------------------------")
     });
-
-} catch (e) {
-    console.log(e);
 }
 
+
+
+// aqui esta la conexion a las bases, ya sea firebase o mongo simplemente comentar y descomentar la variable db.
+
+// let db = "mongo"
+let db = "firebase"
+try {
+    if (db === "mongo") {
+        await dbMN()
+        console.log("Connected to mongo")
+        listen()
+    } else if (db === "firebase") {
+        await dbFB()
+        console.log("Connected to firebase")
+        listen()
+    }
+}
+catch (e) {
+    console.log(e);
+}
--- src/persistencia/dbConnect.js
+++ src/persistencia/dbConnect.js
@@ -1,11 +1,31 @@
 import mongoose from "mongoose";
+import serviceAccount from "./key.json" assert { type: "json"};
+// para dejar mas separado el warnign de node en la consola:
+console.log("")
+console.log("")
+import admin from "firebase-admin"
 
-export async function dbConnect() {
-    const URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0"
-    mongoose.connect(URL)   
+export async function dbMN() {
+    try {
+        const URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0"
+        mongoose.connect(URL)
 
-    mongoose.connection.on(`error`, (err) => {
-        console.log("Error en la conexion: " + err)
-    })
-    
+        mongoose.connection.on(`error`, (err) => {
+            console.log("Error en la conexion: " + err)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+
+export async function dbFB() {
+    try {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+    } catch (e) {
+        console.log(e)
+    }
 }
